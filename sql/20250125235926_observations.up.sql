8c3830af475935ccca09beeedf323fbf45980ac8
Julien Doutre <jul.doutre@gmail.com>
2025-02-09 12:41:50 +0100

    Migrations

---
+++ sql/20250125235926_observations.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS axon.observations (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    asset_type TEXT NOT NULL,
+    asset_id TEXT NOT NULL,
+    attributes JSONB NOT NULL,
+    observer_claims JSONB NOT NULL,
+    timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
