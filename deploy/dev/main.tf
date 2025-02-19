691e436844b784de5bfc210e75282214a2d3c699
Julien Doutre <jul.doutre@gmail.com>
2025-02-19 22:55:46 +0100

    Terraform keycloak config

---
+++ deploy/dev/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    keycloak = {
+      source  = "linz/keycloak"
+      version = "4.4.1"
+    }
+  }
+}
+
+variable "keycloak_http_port" {
+  type = number
+}
+
+variable "keycloak_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+provider "keycloak" {
+  url       = "http://localhost:${var.keycloak_http_port}"
+  client_id = "admin-cli"
+  username  = "admin"
+  password  = var.keycloak_admin_password
+}
+
+data "keycloak_realm" "master" {
+  realm = "master"
+}
+
+resource "keycloak_openid_client" "openid_client" {
+  realm_id  = data.keycloak_realm.master.id
+  client_id = "axon"
+  name      = "axon"
+  enabled   = true
+
+  access_type = "CONFIDENTIAL"
+}
+
+output "openid_client_secret" {
+  value       = keycloak_openid_client.openid_client.client_secret
+  description = "secret"
+  sensitive   = true
+}
